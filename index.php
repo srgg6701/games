349c69e8b6b4d8d96f8ccf6d0fdd73b3e7dd7b4e
srgg6701 <srgg6701@gmail.com>
2013-11-18 00:27:39 +0400

    Создан SASS-проект.

---
+++ index.php
@@ -0,0 +1,11 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Las Vegas</title>
+<link rel="stylesheet" type="text/css" href="stylesheets/screen.css">
+</head>
+<body>
+<div id="screen"></div>
+</body>
+</html>
