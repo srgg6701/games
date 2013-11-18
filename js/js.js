a6c1b5897c6384c5ed96038cb242517d13eed4d8
srgg6701 <srgg6701@gmail.com>
2013-11-18 17:54:16 +0400

    Добавлен паттерн ООП.

--- js/js.js
+++ js/js.js
@@ -1,41 +1,42 @@
 // JavaScript Document
- $(function(){  
-	var d=document;
-	var wrapper=d.getElementById('wrapper');
-	var main=d.getElementById('main');
-	
-	var currentRatio=function(){
-		return d.body.clientWidth/d.body.clientHeight;
+var d=document;
+$(function(){  
+	var game={
+		LasVegas:d.getElementById('wrapper'),
+		zWidth:d.body.clientWidth,
+		zHeight:d.body.clientHeight,
+		ratio:function(){
+			return zWidth/zHeight;
+		}
 	};
+	
 	var setScreenParams = function(){
 		
-		var ratio=currentRatio();
-		
 		var hDiff;
-		if(ratio>=1.33333333333333){ // wide, height = 100%
+		if(game.ratio>=1.33333333333333){ // wide, height = 100%
 			console.log('wide');
-			$(wrapper).outerHeight('100%')
-					  .outerWidth($(wrapper).outerHeight()/6*8)
+			$(game.LasVegas).outerHeight('100%')
+					  .outerWidth($(game.LasVegas).outerHeight()/6*8)
 					  .css({
 						bottom:0,
 						top:0
 					});
-			if(hDiff=d.body.clientWidth-$(wrapper).outerWidth()){
-				$(wrapper).css({
+			if(hDiff=d.body.clientWidth-$(game.LasVegas).outerWidth()){
+				$(game.LasVegas).css({
 					left:hDiff/2
 				});
 				console.log('Выровнять по горизонтали, hDiff/2 = '+(hDiff/2));
 			}
 		}else{	
 			console.log('narrow');
-			$(wrapper).outerWidth('100%') // narrow, width = 100%
-					  .outerHeight($(wrapper).outerWidth()/8*6)
+			$(game.LasVegas).outerWidth('100%') // narrow, width = 100%
+					  .outerHeight($(game.LasVegas).outerWidth()/8*6)
 					  .css({
 						left:0,
 						right:0
 					});
-			if(hDiff=d.body.clientHeight-$(wrapper).outerHeight()){
-				$(wrapper).css({
+			if(hDiff=d.body.clientHeight-$(game.LasVegas).outerHeight()){
+				$(game.LasVegas).css({
 					top:hDiff/2
 				});
 				console.log('Выровнять по вертикали, hDiff/2 = '+(hDiff/2));
@@ -45,5 +46,6 @@
 	setScreenParams();	
 	window.onresize=function(){
 		setScreenParams();
+		//$(game.LasVegas)
 	} 
 });
