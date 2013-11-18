b0f781305ba32786593791f69aee859f6e7c232f
srgg6701 <srgg6701@gmail.com>
2013-11-18 23:28:46 +0400

    Создана подложка для синхронизации, убрана тестовая таблица.

--- index.php
+++ index.php
@@ -22,45 +22,6 @@ table td{
 <body>
 <div id="wrapper">
     <div id="main">
-    <?	//if(isset($_GET['debug'])):
-	?>
-        <table id="tblCalculations">
-        	<tr>
-            	<th id="obj">Object</th>
-            	<th>Height</th>
-            	<th>Width</th>
-            	<th>Ratio</th>
-            </tr>
-        	<tr>
-            	<td>viewport</td>
-            	<td id="vwHeight"></td>
-            	<td id="vwWidth"></td>
-            	<td id="vwRatio"></td>
-            </tr>
-        	<tr>
-        	  <td colspan="4"><img src="sources/pix/538147_344255319014147_1976125150_n.jpg" width="480" height="360"></td>
-       	  </tr>
-        </table>
-        <table>
-          <tr>
-            <td>game Width</td>
-            <td>maxContainerWidth</td>
-            <td>screen.width</td>
-            <td>scale</td>
-            <td>sizes</td>
-          </tr>
-          <tr>
-            <td id="container"></td>
-            <td id="max"></td>
-            <td id="screen"></td>
-            <td id="scale"></td>
-            <td id="sizes"></td>
-          </tr>
-        </table>
-
-    <?	//endif;
-	?>
-    <h1>Hello!</h1>
     </div>
 </div>
 </body>
