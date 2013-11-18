ac407b624cc6a02a218ec9d14e5a3add9f1c8003
srgg6701 <srgg6701@gmail.com>
2013-11-18 12:30:18 +0400

    Переход к новой модели масштабирования.

--- index.php
+++ index.php
@@ -8,52 +8,59 @@
 <script src="js/js.js"></script>
 <script src="js/debug.js"></script>
 <style>
-body{
-	font: normal 100%;
-}
+/*body{
+	font-weight: normal; 
+	font-size:100% !important;
+}*/
+/*
 #screen_wrapper {
     display: inline-block;
+	font-size:2em;
     position: relative;
 	width:100%;		
+}*/
+#spacer{
+	height:100%;
+	width:100%;
 }
+/* 	если "широкий" -  
+	не менее стандартного соотношения (напр. 1027/768; исходный размер макета) */
 @media (min-aspect-ratio: 8/6){ 
 	#screen_wrapper{
 		height:100%;		
 	}
 }
-/*
+
 @media (max-aspect-ratio: 8/6){ 
 	#screen_wrapper{
 		width:100%;		
 	}
-}*/
+}
 
+/*
 #screen_wrapper:after {
-    padding-top: 133.33%; /*ratio*/
-	font-size:2em;
+    padding-top: 133.33%; 
     display: block;
     content: '';
 }
-#screen {
+*/
+/*#screen {
     position: absolute;
 	font-size:1em;
     top: 0; bottom: 0; right: 0; left: 0; 
-}
+}*/
+/*
 #inner_screen{
 	background:#CCFF99;
 	font-size:2em;
 	position: absolute;
 	top:20%; bottom: 20%; right: 20%; left: 20%;
-}
+}*/
 </style>
 </head>
 <body>
 <div id="screen_wrapper">
-    <div id="screen">
-    	<div id="inner_screen">
-        	Hello. I'm inside!
-        </div>
-    </div>
+	<img id="spacer" src="sources/pix/spacer.png" width="240" height="180">
 </div>
 </body>
 </html>
