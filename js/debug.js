37b8c80802381563d3d950108e0b3708369d6655
srgg6701 <srgg6701@gmail.com>
2013-11-28 16:40:22 +0400

    Deleted sources.

--- js/debug.js
+++ js/debug.js
@@ -21,62 +21,65 @@ function getUrlParams(){
 }
 
 $(function(){
-	if(bg=getUrlParams()){
-		var bgs=bg.bg.split(",");
-		//$('#css_screen').after('<link media="screen" href="stylesheets/debug.css" rel="stylesheet" type="text/css">');
-		var op1=parseFloat(bgs[0]/100),
-			op2=parseFloat(bgs[1]/100);
-		
-		var wrapper = $('#wrapper');
-		
-		$('>*',wrapper).css({
-			'opacity':op2
-		});
-		
-		var substrate = $('<div/>',{
-			class:'bg'
-		}).css({
-			background:'url(images/bg_full.jpg)',
-			backgroundSize:'cover',
-			position:'absulute',
-			top:0,
-			right:0,
-			bottom:0,
-			left:0,
-			opacity:op1
-		});
-		
-		$(wrapper).prepend(substrate).css({
-			'background':'none'
-		});
-		
-		var msrH=$('<div/>').css({
-			background:'yellow',
-			position:'absolute',
-			left:0,
-			right:0,
-			height:'1px',
-			zIndex:3
-		});
-		var msrV=$('<div/>').css({
-			background:'orange',
-			position:'absolute',
-			top:0,
-			bottom:0,
-			width:'1px',
-			zIndex:3
-		});
-		
-		$('body').prepend(msrH)
-				 .prepend(msrV)
-			.mousemove(function(e){
-				$(msrH).css({
-					top:e.pageY
-				});
-				$(msrV).css({
-					left:e.pageX
-				});
-				document.title='top: '+e.pageY+', left: '+e.pageX;
+	var bg;
+	if(bg=getUrlParams()){ //console.dir(bg);
+		if (bg.bg){
+			var bgs=bg.bg.split(",");
+			//$('#css_screen').after('<link media="screen" href="stylesheets/debug.css" rel="stylesheet" type="text/css">');
+			var op1=parseFloat(bgs[0]/100),
+				op2=parseFloat(bgs[1]/100);
+			
+			var wrapper = $('#wrapper');
+			
+			$('>*',wrapper).css({
+				'opacity':op2
 			});
+			
+			var substrate = $('<div/>',{
+				class:'bg'
+			}).css({
+				background:'url(sources/bg_full.jpg)',
+				backgroundSize:'cover',
+				position:'absulute',
+				top:0,
+				right:0,
+				bottom:0,
+				left:0,
+				opacity:op1
+			});
+			
+			$(wrapper).prepend(substrate).css({
+				'background':'none'
+			});
+			
+			var msrH=$('<div/>').css({
+				background:'yellow',
+				position:'absolute',
+				left:0,
+				right:0,
+				height:'1px',
+				zIndex:3
+			});
+			var msrV=$('<div/>').css({
+				background:'orange',
+				position:'absolute',
+				top:0,
+				bottom:0,
+				width:'1px',
+				zIndex:3
+			});
+			
+			$('body').prepend(msrH)
+					 .prepend(msrV)
+				.mousemove(function(e){
+					$(msrH).css({
+						top:e.pageY-4
+					});
+					$(msrV).css({
+						left:e.pageX-4
+					});
+					document.title='top: '+e.pageY+', left: '+e.pageX;
+				});
+		}
 	}
 });
